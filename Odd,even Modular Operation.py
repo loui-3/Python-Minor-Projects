c7ef86bcb35ac4387a0211ad774131fbbc47769c
Louiegie Lariosa <louiegie.lariosa@gmail.com>
2025-08-01 16:55:32 +0800

    Update Odd,even Modular Operation.py

--- Odd,even Modular Operation.py
+++ Odd,even Modular Operation.py
@@ -1,6 +1,12 @@
 first_number = int(input("Input a number: "))
+if first_number <= 0:
+    print("The number is zero, cannot perform modulus operation.")
+    exit()
 second_number = int(input("Input a second number: "))
-final_number = int(first_number % second_number)
+if second_number <= 0:
+    print("The number is zero, cannot perform modulus operation.")
+    exit()
+final_number = float(first_number % second_number)
 if final_number == 0 :
     print("The number is Even")
 else:
